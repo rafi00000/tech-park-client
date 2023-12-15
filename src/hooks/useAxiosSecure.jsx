0aa914bbe568de4d27e85963eaa087315d317b5c
Rafi <delwar2021bd@gmail.com>
2023-12-15 18:35:52 +0600

    added axios

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+export const axioss = axios.create({
+    baseURL: "" ,
+    withCredentials: true,
+}) ;
+const useAxiosSecure = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useAxiosSecure;
