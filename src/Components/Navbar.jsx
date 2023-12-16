6438ee4abc10a5a2db550fb26775709b483c255e
rafi00000 <delwar2021bd@gmail.com>
2023-12-16 14:47:14 +0600

    fixed navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -35,12 +35,15 @@ const Navbar = () => {
         <li><NavLink to={'/myCart'}>My Cart</NavLink></li>
         <li><NavLink to={'/login'}>Login</NavLink></li>
         <li><NavLink to={'/category'}>Add Category</NavLink></li>
+        <li><NavLink to={'/feedback'}>Add Category</NavLink></li>
+        { user && <li><NavLink to={'/give-feedback'}>Give Feedback</NavLink></li> }
     </>;
 
   return (
-    <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
+    <div className=" bg-base-100">
+  <div>
+    {/* dropdown for phone here */}
+    <div className="dropdown flex md:hidden">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
@@ -50,18 +53,18 @@ const Navbar = () => {
     </div>
     
     <div className="flex items-center justify-center gap-5">
-      <img src="https://i.ibb.co/XC2KxyC/web-logo.png" className="w-20 hidden md:flex" />
+      <img src="https://i.ibb.co/XC2KxyC/web-logo.png" className="w-20 md:flex" />
     <p className="font-bold text-2xl font-mono hidden md:flex">Tech Park</p>
     </div>
   </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      {navLinks}
-    </ul>
-  </div>
-  <div className="navbar-center md:navbar-end">
+  {/* drop down ends here */}
+
+    <div className="flex justify-center items-center">
+
+      {/* user & dark theme */}
+    <div className="flex items-center gap-4">
     {
-      user? <div className="flex flex-col md:flex-row gap-1 p-1 rounded-xl items-center border">
+      user? <div className="flex flex-col md:flex-row gap-1 rounded-xl items-center border bg-gradient-to-tl from-cyan-500 via-blue-300 to-teal-500 p-2">
         <img src={user.photoURL} alt="" className="w-9 rounded-full"/>
         <p>{user.displayName}</p>
       <button className="btn btn-sm btn-outline" onClick={handleSignOut}>Sign out</button>
@@ -70,6 +73,7 @@ const Navbar = () => {
     }
   <input type="checkbox" className="toggle ml-3" name="theme" onClick={handleThemeChange} />
   </div>
+    </div>
 </div>
   );
 };
